2af8ee216e50668285e39398249b9ae8bd2fbc9a
ma-all <marwaalbaloushi21@gmail.com>
2026-07-09 10:01:47 +0300

    builds server with test route

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('dev'))
+
+app.get('/', async (req, res)=>{
+    res.send('hello')
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+})
